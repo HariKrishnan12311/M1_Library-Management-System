d9500db72127599af4578424eb2b570419fe2979
HariKrishnan12311 <101939465+HariKrishnan12311@users.noreply.github.com>
2022-04-03 15:17:19 +0530

    Add files via upload

---
+++ 3_Implementation/src/Deletebook.c
@@ -0,0 +1,56 @@
+#include<library_Managment_system.h>
+
+/**
+ * Here,we are going to delete the book
+ *  function to remove the records of a book
+ * 
+ *  id 
+ * return test_values 
+ */
+
+test_values deletebook(int id)
+{
+    
+    
+    
+    FILE *fp=NULL;
+    FILE *ft=NULL;
+    fp = fopen("bookLibrary.dat","rb");
+    ft = fopen("temp.dat","wb");
+    
+    if(fp==NULL || ft==NULL)
+        {
+            printf("\nError: While opening file:\n");
+            return fail;
+        }
+    else{
+        rewind(fp);
+        
+        book *discarded_book = (book*)malloc(sizeof(book));
+    while(fread(discarded_book,sizeof(book),1,fp)==1)
+    {
+        if(id != discarded_book->book_id)
+        {
+            
+            fwrite(discarded_book,sizeof(book),1,ft);
+            
+            
+        }
+        
+        
+        
+
+    }
+    fclose(fp);
+    fclose(ft);
+    free(discarded_book);
+  
+   remove("bookLibrary.dat");
+    rename("temp.dat","bookLibrary.dat");
+    
+    return pass;
+    
+    
+        }
+
+}
